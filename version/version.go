16abccb108e4ebe1c0f1245b930433864c73a64a
iseki <admin@iseki.space>
2021-12-10 10:25:44 +0800

    fix: comments & todo

--- version/version.go
+++ version/version.go
@@ -5,12 +5,14 @@ import (
 	"github.com/MakeNowJust/heredoc/v2"
 )
 
-const version = "v"
+const version = "v" // todo: version
 
+// Version returns version string
 func Version() string {
 	return version
 }
 
+// PrintVersionInfo print version info to stdout
 func PrintVersionInfo() {
 	fmt.Printf(heredoc.Doc(`
 	murphysec-cli %s
