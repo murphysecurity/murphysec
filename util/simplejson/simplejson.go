cb29fce622a97eac0c945a9f4c845165d2efde5f
iseki <admin@iseki.space>
2021-12-10 10:23:37 +0800

    fix: add license to simplejson.go

--- util/simplejson/simplejson.go
+++ util/simplejson/simplejson.go
@@ -1,3 +1,30 @@
+/*
+this package is forked from https://github.com/bitly/go-simplejson by MIT license.
+but fix https://github.com/bitly/go-simplejson/issues/42,
+https://github.com/bitly/go-simplejson/issues/41
+https://github.com/bitly/go-simplejson/issues/15,
+https://github.com/bitly/go-simplejson/issues/55.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
 package simplejson
 
 import (
@@ -12,12 +39,6 @@ import (
 	"reflect"
 	"strconv"
 )
-
-// this package is forked from https://github.com/bitly/go-simplejson
-// but fix https://github.com/bitly/go-simplejson/issues/42,
-// https://github.com/bitly/go-simplejson/issues/41
-// https://github.com/bitly/go-simplejson/issues/15,
-// https://github.com/bitly/go-simplejson/issues/55.
 
 type JSON struct {
 	data interface{}
