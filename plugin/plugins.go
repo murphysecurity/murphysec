09537aad3e5efe69dba4b75c3b062f54997cb04c
iseki <admin@iseki.space>
2021-12-14 10:24:41 +0800

    feat: plugin name mapping

--- plugin/plugins.go
+++ plugin/plugins.go
@@ -10,3 +10,15 @@ var Plugins = []plugin_base.Plugin{
 	hello.Instance,
 	gradle.Instance,
 }
+
+var pluginMap = func() map[string]plugin_base.Plugin {
+	m := map[string]plugin_base.Plugin{}
+	for i := range Plugins {
+		m[Plugins[i].Info().Name] = Plugins[i]
+	}
+	return m
+}()
+
+func GetPluginOrNil(name string) plugin_base.Plugin {
+	return pluginMap[name]
+}
