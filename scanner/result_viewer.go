bed19cbcc9d28d7f43125a54426352e04cc61d62
iseki <admin@iseki.space>
2021-12-15 15:13:37 +0800

    fix result_viewer

--- scanner/result_viewer.go
+++ scanner/result_viewer.go
@@ -8,5 +8,6 @@ import (
 func PrintDetectReport(result *api.ScanResult) {
 	fmt.Printf("扫描了 %d 个依赖项，找到了 %d 个问题\n", result.DependenciesCount, result.IssuesCount)
 	ilc := result.IssuesLevelCount
-	fmt.Printf("其中，关键项：%d 高危项：%d 中危项：%d 低微项：%d", ilc.Critical, ilc.High, ilc.Medium, ilc.Low)
+	fmt.Println(fmt.Sprintf("其中，关键项：%d 高危项：%d 中危项：%d 低危项：%d", ilc.Critical, ilc.High, ilc.Medium, ilc.Low))
+	fmt.Println(fmt.Sprintf("任务结果详情请点击：https://www.murphysec.com/control/code-scan/%s", result.TaskId))
 }
