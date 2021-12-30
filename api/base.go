a2664298d2470ef563088da1f76050b9a5429788
iseki <admin@iseki.space>
2021-12-30 22:08:26 +0800

    feat: update default server address

--- api/base.go
+++ api/base.go
@@ -9,7 +9,7 @@ import (
 func serverAddress() string {
 	envServer := strings.Trim(strings.TrimSpace(os.Getenv("MPS_CLI_SERVER")), "/")
 	if len(envServer) == 0 {
-		return "https://sca.murphysec.com"
+		return "https://www.murphysec.com/api"
 	}
 	return envServer
 }
