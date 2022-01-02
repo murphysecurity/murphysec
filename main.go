be3e5614a09c07e811fd915a5564edbd1d2ebec2
iseki <admin@iseki.space>
2022-01-03 01:58:01 +0800

    fix: clean up

--- main.go
+++ main.go
@@ -19,27 +19,3 @@ func main() {
 	r := cmd.RootCmd()
 	_ = r.Execute()
 }
-
-//func main(){
-//	cmd:=exec.Command("cmd", "/c", "echo aa")
-//	stdout, e:=cmd.StdoutPipe()
-//	must.Must(e)
-//	stderr, e:=cmd.StderrPipe()
-//	must.Must(e)
-//	cmd.Start()
-//	time.Sleep(time.Second)
-//	wg:=sync.WaitGroup{}
-//	wg.Add(2)
-//	go func() {
-//		fmt.Println("o", string(must.Byte(ioutil.ReadAll(stdout))))
-//		wg.Done()
-//	}()
-//	go func() {
-//		fmt.Println("e", string(must.Byte(ioutil.ReadAll(stderr))))
-//		wg.Done()
-//	}()
-//
-//	//must.Must(cmd.Start())
-//	wg.Wait()
-//	must.Must(cmd.Wait())
-//}
