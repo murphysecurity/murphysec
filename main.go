2d7299edd0d665106e4ff14deb85cc6d3b2df163
iseki <admin@iseki.space>
2022-01-02 23:19:46 +0800

    feat: add os.Args to log

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"murphysec-cli-simple/cmd"
+	"murphysec-cli-simple/util/output"
+	"os"
+	"strings"
 )
 
 func main() {
+	output.Debug(fmt.Sprintf("CLI arguments: %s", strings.Join(os.Args, " ")))
 	r := cmd.RootCmd()
 	_ = r.Execute()
 }
