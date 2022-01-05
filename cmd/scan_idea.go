a42314660518b4189da4c6863ce3d39145a597ce
iseki <admin@iseki.space>
2022-01-05 15:10:48 +0800

    fix: IDEA scan task type & bump version v1.1.22

--- cmd/scan_idea.go
+++ cmd/scan_idea.go
@@ -45,7 +45,7 @@ func scanByPlugin(p plugin_base.Plugin, dir string) error {
 			CliVersion:         version.Version(),
 			TaskStatus:         1,
 			TaskFailureReason:  "",
-			TaskType:           "JetBrain IDEA",
+			TaskType:           "Jetbrains-IDEA-Plugin",
 			OsType:             runtime.GOOS,
 			CmdLine:            strings.Join(os.Args, " "),
 			Plugin:             p.Info().Name,
