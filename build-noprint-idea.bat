0fca3e59b6c6e34797dc4ce07795b249db1ce2ae
iseki <admin@iseki.space>
2022-01-14 14:25:18 +0800

    fix: remove useless build script

--- build-noprint-idea.bat
+++
@@ -1,6 +0,0 @@
-set GOOS=windows
-go build -o murphysec-windows.exe -tags noprint,idea .
-set GOOS=linux
-go build -o murphysec-linux -tags noprint,idea
-set GOOS=darwin
-go build -o murphysec-macos -tags noprint,idea .
