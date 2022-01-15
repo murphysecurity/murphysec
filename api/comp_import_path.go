25299fe0a11ef4910d1672cb13fed8607a0c9c92
iseki <admin@iseki.space>
2022-01-15 19:05:27 +0800

    fix: API result

--- api/comp_import_path.go
+++ api/comp_import_path.go
@@ -8,19 +8,21 @@ import (
 	"net/http"
 )
 
-func CompImportPath(data interface{}){
-	uri:=serverAddress()+"/api/v1/access/detect/import_path"
+func CompImportPath(data interface{}) {
+	uri := serverAddress() + "/api/v1/access/detect/import_path"
 	logger.Info.Println("Call API:", uri)
 	logger.Debug.Println("API request body:")
-	body:=must.Byte(json.Marshal(data))
+	body := must.Byte(json.Marshal(data))
 	logger.Debug.Println(string(body))
-	req, e:=http.NewRequest(http.MethodPost, uri, bytes.NewReader(body))
+	req, e := http.NewRequest(http.MethodPost, uri, bytes.NewReader(body))
 	must.Must(e)
-	res, e:=client.Do(req)
-	if e!=nil{
+	res, e := client.Do(req)
+	if e != nil {
 		logger.Err.Println("API request, failed.", e.Error())
 	}
-	if res.StatusCode!=http.StatusOK{
+	if res.StatusCode != http.StatusOK {
 		logger.Err.Println("API status code!=OK, code:", res.StatusCode)
+	} else {
+		logger.Info.Println("API request succeed")
 	}
 }
