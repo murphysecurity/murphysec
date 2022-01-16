d7c29e6dcd7ac7f9e8d0a33b69d8769863d278d3
iseki <admin@iseki.space>
2022-01-16 18:38:53 +0800

    feat: cli scan

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"murphysec-cli-simple/inspector"
+	"murphysec-cli-simple/logger"
+	"murphysec-cli-simple/utils/must"
+	"path/filepath"
 )
 
 func scanCmd() *cobra.Command {
@@ -10,9 +15,15 @@ func scanCmd() *cobra.Command {
 		Run: scanRun,
 	}
 	c.Args = cobra.ExactArgs(1)
+	c.Args = cobra.NoArgs
 	return c
 }
 
 func scanRun(cmd *cobra.Command, args []string) {
-
+	_, e := inspector.CliScan(must.String(filepath.Abs(args[0])))
+	if e != nil {
+		SetGlobalExitCode(1)
+		fmt.Println("Scan failed.")
+		logger.Err.Println("cli scan failed.", e.Error())
+	}
 }
---
+++ inspector/scan.go
@@ -0,0 +1,46 @@
+package inspector
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/pkg/errors"
+	"murphysec-cli-simple/api"
+	"murphysec-cli-simple/logger"
+	"murphysec-cli-simple/module/base"
+	"time"
+)
+
+func CliScan(dir string) (interface{}, error) {
+	startTime := time.Now()
+	engine := tryMatchInspector(dir)
+	if engine == nil {
+		return nil, errors.New("Can't inspect project. No inspector supported.")
+	}
+	// 开始扫描
+	logger.Info.Println("IdeaScan dir:", dir, "Inspector:", engine.String(), "Time:", startTime.Format(time.RFC3339))
+	modules, e := engine.Inspect(dir)
+	if e != nil {
+		return nil, errors.Wrap(e, "Engine scan failed.")
+	}
+	req := getIdeaRequest()
+	// 拼凑项目信息
+	wrapProjectInfoToReqObj(req, dir)
+	logger.Debug.Println("Before scan. projectName:", req.ProjectName, "git:", req.GitInfo != nil)
+	// 拼凑请求体 模块
+	moduleUUIDMap := map[uuid.UUID]base.Module{}
+	for _, it := range modules {
+		moduleVo := mapVoModule(it)
+		moduleVo.ModuleUUID = uuid.Must(uuid.NewRandom())
+		moduleUUIDMap[moduleVo.ModuleUUID] = it
+		req.Modules = append(req.Modules, moduleVo)
+	}
+	// API 请求
+	r, e := api.SendDetect(*req)
+	if e != nil {
+		return nil, errors.Wrap(e, "Server request failed.")
+	}
+	// 输出 API 响应
+	fmt.Println(fmt.Sprintf("扫描完成，共计%d个组件，%d个漏洞", r.DependenciesCount, r.IssuesCompsCount))
+	javaImportClauseScan(r, dir)
+	return nil, nil
+}
