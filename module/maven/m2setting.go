5a62b42dd33b73a804c660dc0f77e7d842b3aad4
iseki <admin@iseki.space>
2022-02-21 00:23:59 +0800

    refactor: maven settings.xml read

--- module/maven/m2setting.go
+++ module/maven/m2setting.go
@@ -4,27 +4,28 @@ import (
 	"bytes"
 	"github.com/antchfx/xmlquery"
 	"github.com/mitchellh/go-homedir"
+	"github.com/pkg/errors"
 	"murphysec-cli-simple/logger"
 	"murphysec-cli-simple/utils/must"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 func ReadM2SettingMirror() *M2Setting {
-	// todo: refactor
 	rs := &M2Setting{
 		Mirrors:  nil,
 		RepoPath: filepath.Join(must.String(homedir.Dir()), ".m2", "repository"),
 	}
-	settingPath := filepath.Join(must.String(homedir.Dir()), ".m2", "settings.xml")
-	if t := os.Getenv("MPS_CLI_M2_SETTINGS"); t != "" {
-		settingPath = t
-	}
-	logger.Info.Println("Read maven settings.xml from", settingPath)
-	data, e := os.ReadFile(settingPath)
+	var data []byte
+	var e error
+	data, e = readUserHomeM2Settings()
 	if e != nil {
-		logger.Err.Println("Read settings failed.", e.Error())
-		return nil
+		logger.Info.Println("Read user home maven settings.xml failed.", e.Error())
+		data, e = readMvnInstallPathSettingsFile()
+		if e != nil {
+			logger.Info.Println("Read maven install settings.xml failed.", e.Error())
+		}
 	}
 	node, e := xmlquery.Parse(bytes.NewReader(data))
 	if e != nil {
@@ -47,4 +48,33 @@ func ReadM2SettingMirror() *M2Setting {
 type M2Setting struct {
 	Mirrors  []string
 	RepoPath string
+}
+
+func locateMvnInstallPath() string {
+	for _, it := range strings.Split(os.Getenv("PATH"), string(os.PathListSeparator)) {
+		fp, e := filepath.EvalSymlinks(filepath.Join(it, "mvn"))
+		if e != nil {
+			continue
+		}
+		return fp
+	}
+	return ""
+}
+
+func readUserHomeM2Settings() ([]byte, error) {
+	if hd, e := homedir.Dir(); e != nil {
+		return nil, e
+	} else {
+		p := filepath.Join(hd, ".m2", "settings.xml")
+		return os.ReadFile(p)
+	}
+}
+
+func readMvnInstallPathSettingsFile() ([]byte, error) {
+	mvnBin := locateMvnInstallPath()
+	if mvnBin == "" {
+		return nil, errors.New("mvn binary not found")
+	}
+	p := filepath.Join(filepath.Base(filepath.Base(mvnBin)), "conf", "settings.xml")
+	return os.ReadFile(p)
 }
