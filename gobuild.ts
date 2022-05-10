c2cfd322beee25e44e72f30558cad436ad7b0e76
iseki <admin@iseki.space>
2022-05-10 15:20:30 +0800

    fix: ci script indent

--- gobuild.ts
+++ gobuild.ts
@@ -1,7 +1,7 @@
 const targets: [string, string][] = [
-  ["linux", "amd64"],
-  ["darwin", "amd64"],
-  ["windows", "amd64"],
+    ["linux", "amd64"],
+    ["darwin", "amd64"],
+    ["windows", "amd64"],
 ];
 
 const isSaaS = Deno.args.indexOf("saas") > -1;
@@ -9,25 +9,23 @@ const bn = isSaaS ? "murphysec-saas" : "murphysec";
 
 const tags = [!isSaaS ? "pro" : ""].filter((it) => it);
 const opts = targets.map((it) => ({
-  cmd: [
-    "go",
-    "build",
-    tags.length > 0 ? ["-tags", ...tags] : [],
-    "-o",
-    `out/${bn}-${it[0]}-${it[1]}${it[0] === "windows" ? ".exe" : ""}`,
-    ".",
-  ]
-    .flat()
-    .filter((it) => it),
-  env: { GOOS: it[0], GOARCH: it[1] },
-  stdin: "null" as "null",
+    cmd: [
+        "go",
+        "build",
+        tags.length > 0 ? ["-tags", ...tags] : [],
+        "-o",
+        `out/${bn}-${it[0]}-${it[1]}${it[0] === "windows" ? ".exe" : ""}`,
+        ".",
+    ].flat().filter((it) => it),
+    env: {GOOS: it[0], GOARCH: it[1]},
+    stdin: "null" as "null",
 }));
 console.log(opts);
 const process = opts.map((it) => Deno.run(it).status());
 const status = await Promise.all(process);
 if (status.every((it) => it.success && it.code === 0)) {
-  console.log("编译成功");
+    console.log("编译成功");
 } else {
-  console.error("编译失败", status);
-  Deno.exit(-1);
+    console.error("编译失败", status);
+    Deno.exit(-1);
 }
