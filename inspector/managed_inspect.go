fd4e4fc013e49621e561ddb24cd3de96960df347
iseki <admin@iseki.space>
2022-05-20 13:45:54 +0800

    feat: recursive scan depth 3

--- inspector/managed_inspect.go
+++ inspector/managed_inspect.go
@@ -41,7 +41,7 @@ func managedInspectScan(ctx *ScanContext) error {
 			}
 			{
 				s, e := filepath.Rel(ctx.ProjectDir, path)
-				if strings.Count(filepath.ToSlash(s), "/") > 5 || e != nil {
+				if strings.Count(filepath.ToSlash(s), "/") > 3 || e != nil {
 					return filepath.SkipDir
 				}
 			}
