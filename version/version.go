bd2dc397e416aef816a2fd90ee332bb428d2dc94
iseki <admin@iseki.space>
2022-06-28 12:07:32 +0800

    refactor(common): generate user-agent string in init function

--- version/version.go
+++ version/version.go
@@ -3,11 +3,9 @@ package version
 import (
 	"fmt"
 	"github.com/iseki0/osname"
-	"github.com/murphysecurity/murphysec/logger"
 	"github.com/murphysecurity/murphysec/utils/must"
 	"os"
 	"path/filepath"
-	"sync"
 )
 
 const version = "v1.7.6"
@@ -17,22 +15,16 @@ func PrintVersionInfo() {
 	fmt.Printf("%s %s\n", filepath.Base(must.A(filepath.EvalSymlinks(must.A(os.Executable())))), Version())
 }
 
-var _ua = func() func() string {
-	o := sync.Once{}
-	ua := ""
+var userAgent string
+
+func init() {
 	osn, e := osname.OsName()
 	if e != nil {
 		osn = "<unknownOS>"
 	}
-	return func() string {
-		o.Do(func() {
-			ua = fmt.Sprintf("murphysec-cli/%s (%s);", Version(), osn)
-			logger.Debug.Println("user-agent:", ua)
-		})
-		return ua
-	}
-}()
+	userAgent = fmt.Sprintf("murphysec-cli/%s (%s);", Version(), osn)
+}
 
 func UserAgent() string {
-	return _ua()
+	return userAgent
 }
