8e1f086cc6879203f1c5270024cb8a841c6ca4b9
iseki <admin@iseki.space>
2022-08-05 13:14:44 +0800

    fix(composer): Use `exec.LookPath` finding composer command before use it

--- module/composer/composer_helper.go
+++ module/composer/composer_helper.go
@@ -2,14 +2,20 @@ package composer
 
 import (
 	"context"
+	"github.com/murphysecurity/murphysec/errors"
 	"github.com/murphysecurity/murphysec/utils"
-	"github.com/pkg/errors"
 	"os/exec"
 )
 
+var ErrNoComposerFound = errors.New("no composer found")
+
 func doComposerInstall(ctx context.Context, projectDir string) error {
+	composerPath, e := exec.LookPath("composer")
+	if e != nil {
+		return errors.WithCause(ErrNoComposerFound, e)
+	}
 	logger := utils.UseLogger(ctx)
-	c := exec.CommandContext(ctx, "composer", "install", "--ignore-platform-reqs", "--no-progress", "--no-dev", "--no-autoloader", "--no-scripts", "--no-interaction")
+	c := exec.CommandContext(ctx, composerPath, "install", "--ignore-platform-reqs", "--no-progress", "--no-dev", "--no-autoloader", "--no-scripts", "--no-interaction")
 	c.Dir = projectDir
 	logger.Sugar().Infof("Command: %s", c.String())
 	defer logger.Info("doComposerInstall terminated")
@@ -18,7 +24,7 @@ func doComposerInstall(ctx context.Context, projectDir string) error {
 	c.Stderr = lp
 	c.Stdout = lp
 	if e := c.Run(); e != nil {
-		return errors.WithMessage(e, "composer install command execute failed")
+		return errors.Wrap(e, "composer install command execute failed")
 	}
 	return nil
 }
