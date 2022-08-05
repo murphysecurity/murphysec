9f501b206292efe15b990a70a4523795f477a042
iseki <admin@iseki.space>
2022-08-05 14:05:54 +0800

    test(conan): Skip conan command test in CI environment if conan not installed

--- module/conan/conan_cmd_test.go
+++ module/conan/conan_cmd_test.go
@@ -3,9 +3,17 @@ package conan
 import (
 	"context"
 	"github.com/murphysecurity/murphysec/utils/must"
+	"github.com/pkg/errors"
+	"os"
+	"os/exec"
 	"testing"
 )
 
 func TestGetConanVersion(t *testing.T) {
+	_, e := exec.LookPath("conan")
+	if os.Getenv("CI") != "" && errors.Is(e, exec.ErrNotFound) {
+		t.Skip("Conan not found in CI environment, test skipped.")
+		return
+	}
 	t.Log(GetConanVersion(context.TODO(), must.A(LocateConan(context.TODO()))))
 }
