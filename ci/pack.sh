f1dc516eb91aa7dd4aeb3448c8543a76d659fa0a
iseki <admin@iseki.space>
2022-08-08 10:44:15 +0800

    ci: Fix bash shell

--- ci/pack.sh
+++ ci/pack.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
 cd out
-#sleep 1h
-if [ $(echo $CI_BUILD_REF_NAME | grep -o '^v' | wc -l) -eq 1 ]; then
+if [ $(echo $CI_DEBUG_DELAY) ]; then
+  echo Sleep for debug
+  sleep 1h
+fi
+if [ $(echo $CI_BUILD_REF_NAME | grep -o '^v') ]; then
   echo Packaging for IDEA Plugin
   f_prefix=$(echo $CI_BUILD_REF_NAME | sed -r 's/(^v|\.)//g')
   echo Name prefix: $f_prefix
@@ -12,8 +15,8 @@ if [ $(echo $CI_BUILD_REF_NAME | grep -o '^v' | wc -l) -eq 1 ]; then
   ideaFile="$f_prefix-murphysec-saas-linux-amd64 $f_prefix-murphysec-saas-darwin-amd64 $f_prefix-murphysec-saas-windows-amd64.exe"
   zip idea.zip $ideaFile
   echo Uploading...
-  cos-uploader --local pro.zip --remote /client/$CI_BUILD_REF_NAME/idea.zip
-  cos-uploader --local pro.zip --remote /client/-/idea.zip
+  cos-uploader --local idea.zip --remote /client/$CI_BUILD_REF_NAME/idea.zip
+  cos-uploader --local idea.zip --remote /client/-/idea.zip
 fi
 
 saasFile='murphysec-saas-linux-amd64 murphysec-saas-darwin-amd64 murphysec-saas-windows-amd64.exe'
