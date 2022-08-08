315bd2266650693e767b70d15704602dbf7eb978
iseki <admin@iseki.space>
2022-08-08 13:22:25 +0800

    ci: Fix bash shell

--- ci/pack.sh
+++ ci/pack.sh
@@ -5,6 +5,10 @@ if [ $(echo $CI_DEBUG_DELAY) ]; then
   echo Sleep for debug
   sleep 1h
 fi
+
+saasFile='murphysec-saas-linux-amd64 murphysec-saas-darwin-amd64 murphysec-saas-windows-amd64.exe'
+proFile='murphysec-linux-amd64 murphysec-darwin-amd64 murphysec-windows-amd64.exe'
+
 if [ $(echo $CI_BUILD_REF_NAME | grep -o '^v') ]; then
   echo Packaging for IDEA Plugin
   f_prefix=$(echo $CI_BUILD_REF_NAME | sed -r 's/(^v|\.)//g')
@@ -19,10 +23,12 @@ if [ $(echo $CI_BUILD_REF_NAME | grep -o '^v') ]; then
   cos-uploader --local idea.zip --remote /client/-/idea.zip
 fi
 
-saasFile='murphysec-saas-linux-amd64 murphysec-saas-darwin-amd64 murphysec-saas-windows-amd64.exe'
-proFile='murphysec-linux-amd64 murphysec-darwin-amd64 murphysec-windows-amd64.exe'
 zip saas.zip $saasFile
 zip pro.zip $proFile
+echo -n $saasFile | xargs -I % -d ' ' cos-uploader --local % --remote /client/$CI_BUILD_REF_NAME/%
+echo -n $saasFile | xargs -I % -d ' ' cos-uploader --local % --remote /client/-/%
+echo -n $proFile | xargs -I % -d ' ' cos-uploader --local % --remote /client/$CI_BUILD_REF_NAME/%
+echo -n $proFile | xargs -I % -d ' ' cos-uploader --local % --remote /client/-/%
 cos-uploader --local pro.zip --remote /client/$CI_BUILD_REF_NAME/pro.zip
 cos-uploader --local pro.zip --remote /client/-/pro.zip
 cos-uploader --local saas.zip --remote /client/$CI_BUILD_REF_NAME/saas.zip
