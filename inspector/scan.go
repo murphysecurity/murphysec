28a1f40122d26b9e83c3ac4a1edb3a245a04373c
iseki <admin@iseki.space>
2022-08-16 12:53:02 +0800

    Revert "fix(inspector): 扫描文件哈希无论是否有模块被找到"
    This reverts commit 3b7853fc983e6e93dd34a48e388c713b632bbe07.

--- inspector/scan.go
+++ inspector/scan.go
@@ -26,7 +26,7 @@ func Scan(ctx context.Context) error {
 		return e
 	}
 
-	if env.AllowFileHash {
+	if env.AllowFileHash && len(scanTask.Modules) == 0 {
 		Logger.Info("File hash scanning")
 		if e := FileHashScan(ctx); e != nil {
 			Logger.Error("FileHash calc failed", zap.Error(e))
