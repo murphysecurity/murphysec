f9f84c44182e69fb3dfa96c31dc9462ab020caec
iseki <admin@iseki.space>
2022-09-20 16:04:20 +0800

    ci: Remove idea packages

--- ci/pack.sh
+++ ci/pack.sh
@@ -9,20 +9,6 @@ fi
 saasFile='murphysec-saas-linux-amd64 murphysec-saas-darwin-amd64 murphysec-saas-windows-amd64.exe'
 proFile='murphysec-linux-amd64 murphysec-darwin-amd64 murphysec-windows-amd64.exe'
 
-if [ $(echo $CI_BUILD_REF_NAME | grep -o '^v') ]; then
-  echo Packaging for IDEA Plugin
-  f_prefix=$(echo $CI_BUILD_REF_NAME | sed -r 's/(^v|\.)//g')
-  echo Name prefix: $f_prefix
-  cp murphysec-saas-linux-amd64 $f_prefix-murphysec-saas-linux-amd64
-  cp murphysec-saas-darwin-amd64 $f_prefix-murphysec-saas-darwin-amd64
-  cp murphysec-saas-windows-amd64.exe $f_prefix-murphysec-saas-windows-amd64.exe
-  ideaFile="$f_prefix-murphysec-saas-linux-amd64 $f_prefix-murphysec-saas-darwin-amd64 $f_prefix-murphysec-saas-windows-amd64.exe"
-  zip idea.zip $ideaFile
-  echo Uploading...
-  cos-uploader --local idea.zip --remote /client/$CI_BUILD_REF_NAME/idea.zip
-  cos-uploader --local idea.zip --remote /client/-/idea.zip
-fi
-
 zip saas.zip $saasFile
 zip pro.zip $proFile
 echo -n $saasFile | xargs -I % -d ' ' cos-uploader --local % --remote /client/$CI_BUILD_REF_NAME/%
