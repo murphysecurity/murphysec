edbdf56973103a43e68e3b03c2c8f618e44e5c7c
iseki <admin@iseki.space>
2022-09-26 21:26:59 +0800

    test(maven): Test_ParseMvnVersion for `parseMvnVersion`

--- module/maven/mvn_cmd_utils_test.go
+++ module/maven/mvn_cmd_utils_test.go
@@ -13,3 +13,15 @@ func TestCheckMvnCommand(t *testing.T) {
 		t.Log(info.String())
 	}
 }
+
+func Test_ParseMvnVersion(t *testing.T) {
+	s := `Apache Maven 3.8.6 (84538c9988a25aec085021c365c560670ad80f63)
+Maven home: C:\Users\iseki\scoop\apps\maven\current
+Java version: 1.8.0_345, vendor: Azul Systems, Inc., runtime: C:\Program Files\Zulu\zulu-8\jre
+Default locale: zh_CN, platform encoding: GBK
+OS name: "windows 10", version: "10.0", arch: "amd64", family: "windows"
+
+`
+	v := parseMvnVersion(s)
+	assert.Equal(t, "3.8.6", v)
+}
