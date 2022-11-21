cf5f75144d61ab3e3dd05620450659faadb89a55
iseki <admin@iseki.space>
2022-11-21 11:38:08 +0800

    feat: httplogger

---
+++ infra/httplogger/httplogger.go
@@ -0,0 +1,48 @@
+package httplogger
+
+import (
+	"go.uber.org/zap"
+	"net/http"
+	"net/http/httputil"
+)
+
+type middlewareWithZapLogger struct {
+	Tripper http.RoundTripper
+	Logger  *zap.Logger
+}
+
+func (m *middlewareWithZapLogger) RoundTrip(request *http.Request) (resp *http.Response, e error) {
+	var dump []byte
+	dump, e = httputil.DumpRequestOut(request, true)
+	if e != nil {
+		m.Logger.Error("dump request out failed", zap.Error(e))
+	} else {
+		m.Logger.Debug("Http request", zap.ByteString("dump", dump))
+	}
+	resp, e = m.Tripper.RoundTrip(request)
+	if e != nil {
+		return
+	}
+	dump, e = httputil.DumpResponse(resp, true)
+	if e != nil {
+		m.Logger.Error("dump response out failed", zap.Error(e))
+	} else {
+		m.Logger.Debug("http response", zap.ByteString("dump", dump))
+	}
+	return
+}
+
+var _ http.RoundTripper = (*middlewareWithZapLogger)(nil)
+
+func New(tripper http.RoundTripper, logger *zap.Logger) http.RoundTripper {
+	if tripper == nil {
+		panic("tripper == nil")
+	}
+	if logger == nil {
+		panic("logger == nil")
+	}
+	return &middlewareWithZapLogger{
+		Tripper: tripper,
+		Logger:  logger,
+	}
+}
