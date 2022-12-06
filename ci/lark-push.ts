c0427baf92d1f0bd2f3d098c26742f3378aa7fea
iseki <admin@iseki.space>
2022-12-06 17:56:06 +0800

    ci: internal upload
    Squashed commit of the following:
    
    commit 2abea6e0931cf5c8b4e57b530d449dd8e9a1366d
    Author: iseki <admin@iseki.space>
    Date:   Tue Dec 6 17:50:45 2022 +0800
    
        ci: internal upload
    
    commit 74fd99b9a7912d305ea369af0d1b8f5cf9a0482a
    Author: iseki <admin@iseki.space>
    Date:   Tue Dec 6 17:47:37 2022 +0800
    
        ci: internal upload
    
    commit af37e951ab5eeb3985ed3c8efa7c5d19198012b0
    Author: iseki <admin@iseki.space>
    Date:   Tue Dec 6 17:42:52 2022 +0800
    
        ci: internal upload
    
    commit f87fe491dc3c8ed14bb6f7729df935d564e240ad
    Author: iseki <admin@iseki.space>
    Date:   Tue Dec 6 17:36:24 2022 +0800
    
        ci: internal upload
    
    commit 4ded80703d60575ab94e3b8d6e0011997f81635d
    Author: iseki <admin@iseki.space>
    Date:   Tue Dec 6 17:11:19 2022 +0800
    
        ci: internal upload
    
    commit 66020d061b54555a88a9d6b0e806e7ac1c3cc74c
    Author: iseki <admin@iseki.space>
    Date:   Tue Dec 6 17:03:21 2022 +0800
    
        ci: internal upload

---
+++ ci/lark-push.ts
@@ -0,0 +1,38 @@
+// import {Sha256} from "https://deno.land/std@0.159.0/hash/sha256.ts"
+
+const larkPushKey = Deno.env.get("LARK_PUSH_KEY")
+const qCloudUrl = (fn) => `https://${Deno.env.get('QCLOUD_COS_DOMAIN')}/client/${Deno.env.get('GITHUB_REF_NAME')}/${fn}`
+const main = async () => {
+
+    const r = await fetch(
+        "https://open.feishu.cn/open-apis/bot/v2/hook/" + larkPushKey,
+        {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({msg_type: "interactive", card: data}),
+        }
+    );
+}
+
+const sign = Deno.env.get('GITHUB_REF').startsWith('refs/tag')?'🔖':'✔'
+
+const data = {
+    "config": {
+        "wide_screen_mode": true
+    },
+    "elements": [
+        {
+            "tag": "markdown",
+            "content": `**📦Bundle：** ${qCloudUrl('bundle.7z')}`
+        }
+    ],
+    "header": {
+        "template": "green",
+        "title": {
+            "content": `${sign}Client - ${Deno.env.get('GITHUB_REF_NAME')}`,
+            "tag": "plain_text"
+        }
+    }
+}
+
+await main()
