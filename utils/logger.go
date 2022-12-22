fec422ed12deae3e71b0d4cff1024dfcdb6bb362
iseki <admin@iseki.space>
2022-12-22 18:21:13 +0800

    refactor: modify utils/logger.go for compatible reason

--- utils/logger.go
+++ utils/logger.go
@@ -2,19 +2,15 @@ package utils
 
 import (
 	"context"
+	"github.com/murphysecurity/murphysec/infra/logctx"
 	"go.uber.org/zap"
 )
 
-const _LoggerCtxKey = `_LoggerCtxKey`
-
 func WithLogger(ctx context.Context, logger *zap.Logger) context.Context {
-	return context.WithValue(ctx, _LoggerCtxKey, logger)
+	return logctx.With(ctx, logger)
 }
 
 // UseLogger returns *zap.Logger in the context. If no Logger exists, returns zap.NewNop()
 func UseLogger(ctx context.Context) *zap.Logger {
-	if l, ok := ctx.Value(_LoggerCtxKey).(*zap.Logger); ok {
-		return l
-	}
-	return zap.NewNop()
+	return logctx.Use(ctx)
 }
