7cc2c2f9c21b8132afc482fdce329a08af1d0f10
iseki <admin@iseki.space>
2023-01-04 20:21:26 +0800

    ci(gitlab): remove saas build

--- ci/pack.sh
+++ ci/pack.sh
@@ -8,12 +8,9 @@ fi
 
 cd out/bin
 
-saasFile='murphysec-saas-linux-amd64 murphysec-saas-darwin-amd64 murphysec-saas-windows-amd64.exe'
 proFile='murphysec-linux-amd64 murphysec-darwin-amd64 murphysec-windows-amd64.exe'
 
 mkdir -p ../zip
-zip saas.zip $saasFile
-mv saas.zip ../zip/
 zip pro.zip $proFile
 mv pro.zip ../zip
 
@@ -21,8 +18,6 @@ cd ../zip
 
 cos-uploader --local pro.zip --remote /client/$CI_BUILD_REF_NAME/pro.zip
 cos-uploader --local pro.zip --remote /client/-/pro.zip
-cos-uploader --local saas.zip --remote /client/$CI_BUILD_REF_NAME/saas.zip
-cos-uploader --local saas.zip --remote /client/-/saas.zip
 echo ================ SHA-256 ================
 sha256sum *
 
