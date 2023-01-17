f16fd8cbb46071ad99e379e73c44cfb6e0fe3c2b
iseki <admin@iseki.space>
2023-01-17 11:31:41 +0800

    refactor(main): move entrypoint to root

--- cmd/murphy/root.go
+++ cmd/murphy/root.go
@@ -1,4 +1,4 @@
-package main
+package murphy
 
 import (
 	"fmt"
--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/murphysecurity/murphysec/cmd/murphy"
 	"github.com/murphysecurity/murphysec/infra/exitcode"
 	"os"
 )
 
 func main() {
-	if e := rootCmd().Execute(); e != nil {
+	if e := murphy.Execute(); e != nil {
 		fmt.Fprintln(os.Stderr, e.Error())
 		os.Exit(1)
 	}
