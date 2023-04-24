fbed5d9937e94aede7f50d660a6c480dcae391fe
wangxiaodong <645775992@qq.com>
2023-04-24 17:12:44 +0800

    版本脚本

---
+++ utils/hash_test.go
@@ -0,0 +1,87 @@
+package utils
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+//GOOS=windows GOARCH=amd64 go build -v -ldflags "-s -w -X github.com/murphysecurity/murphysec/infra/buildinfo.version=$CI_COMMIT_TAG -buildid=" -trimpath -o out/bin/murphysec-windows-amd64.exe
+//GOOS=linux GOARCH=amd64 go build -v -ldflags "-s -w -X github.com/murphysecurity/murphysec/infra/buildinfo.version=$CI_COMMIT_TAG -buildid=" -trimpath -o out/bin/murphysec-linux-amd64
+//GOOS=linux GOARCH=arm64 go build -v -ldflags "-s -w -X github.com/murphysecurity/murphysec/infra/buildinfo.version=$CI_COMMIT_TAG -buildid=" -trimpath -o out/bin/murphysec-linux-arm64
+//GOOS=darwin GOARCH=amd64 go build -v -ldflags "-s -w -X github.com/murphysecurity/murphysec/infra/buildinfo.version=$CI_COMMIT_TAG -buildid=" -trimpath -o out/bin/murphysec-darwin-amd64
+//GOOS=darwin GOARCH=arm64 go build -v -ldflags "-s -w -X github.com/murphysecurity/murphysec/infra/buildinfo.version=$CI_COMMIT_TAG -buildid=" -trimpath -o out/bin/murphysec-darwin-arm64
+
+func TestHash(t *testing.T) {
+	dir, _ := os.Getwd()
+	all := strings.ReplaceAll(dir, "utils", filepath.Join("out", "bin"))
+	fileSystem := os.DirFS(all)
+	if fileSystem != nil {
+		cliVersion := "302-"
+		tt := TT{
+			PluginVersion: "3.0.6",
+		}
+
+		fs.WalkDir(fileSystem, ".", func(path string, d fs.DirEntry, err error) error {
+			if d.IsDir() {
+				return nil
+			}
+
+			file, err := os.Open(filepath.Join(all, d.Name()))
+			hash := md5.New()
+			if _, err := io.Copy(hash, file); err != nil {
+				fmt.Println(err)
+				return nil
+			}
+
+			// 计算哈希值并将其转换为十六进制字符串
+			md5Hash := hex.EncodeToString(hash.Sum(nil))
+
+			typ := ""
+			if strings.Contains(d.Name(), "murphysec-windows") {
+				typ = "win-amd64"
+			} else if strings.Contains(d.Name(), "murphysec-darwin-amd64") {
+				typ = "mac-amd64"
+			} else if strings.Contains(d.Name(), "murphysec-darwin-arm64") {
+				typ = "mac-arm64"
+
+			} else if strings.Contains(d.Name(), "murphysec-linux-amd64") {
+				typ = "linux-amd64"
+
+			} else if strings.Contains(d.Name(), "murphysec-linux-arm64") {
+				typ = "linux-arm64"
+
+			}
+
+			detail := Detail{
+				Type:     typ,
+				FileName: cliVersion + d.Name(),
+				Hash:     md5Hash,
+			}
+			tt.Details = append(tt.Details, detail)
+
+			return nil
+		})
+
+		marshal, _ := json.Marshal(tt)
+		println(string(marshal))
+	}
+
+}
+
+type TT struct {
+	PluginVersion string   `json:"plugin_version"`
+	Details       []Detail `json:"details"`
+}
+type Detail struct {
+	Type     string `json:"type"`
+	FileName string `json:"file_name"`
+	Hash     string `json:"hash"`
+}
