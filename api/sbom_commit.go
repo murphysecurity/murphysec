16c38fb07c42c9978120561fa58177c27c8e70eb
iseki <admin@iseki.space>
2023-04-27 10:57:43 +0800

    style: remove unused code

--- api/sbom_commit.go
+++ api/sbom_commit.go
@@ -6,27 +6,6 @@ import (
 	"github.com/murphysecurity/murphysec/utils/must"
 )
 
-type SubmitSBOMTreeNode struct {
-	Dependencies []SubmitSBOMTreeNode `json:"dependencies,omitempty"`
-	CompName     string               `json:"comp_name"`
-	CompVersion  string               `json:"comp_version"`
-	Ecosystem    string               `json:"ecosystem"`
-	Repository   string               `json:"repository"`
-}
-
-type SubmitSBOMModule struct {
-	ModuleName     string               `json:"module_name"`
-	ModuleVersion  string               `json:"module_version"`
-	ModulePath     string               `json:"module_path"`
-	PackageManager string               `json:"package_manager"`
-	Dependencies   []SubmitSBOMTreeNode `json:"dependencies,omitempty"`
-}
-
-type SBOMSubmitRequest struct {
-	SubtaskId string             `json:"subtask_id"`
-	Modules   []SubmitSBOMModule `json:"modules"`
-}
-
 func SubmitSBOM(client *Client, subtaskId string, modules []model.Module, codeFragments []model.ComponentCodeFragment) error {
 	checkNotNull(client)
 	must.NotZero(subtaskId)
