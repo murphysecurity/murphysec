6a2d7b65fec6049fa925de3350300edbde441025
iseki <admin@iseki.space>
2023-06-06 15:12:55 +0800

    feat(maven): add DO_NOT_SCAN_MAVEN environment

--- module/maven_init.go
+++ module/maven_init.go
@@ -1,7 +1,13 @@
 package module
 
-import "github.com/murphysecurity/murphysec/module/maven"
+import (
+	"github.com/murphysecurity/murphysec/module/maven"
+	"os"
+)
 
 func init() {
+	if os.Getenv("DO_NOT_SCAN_MAVEN") == "1" {
+		return
+	}
 	Inspectors = append(Inspectors, &maven.Inspector{})
 }
