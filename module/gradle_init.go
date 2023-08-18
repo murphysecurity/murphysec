c2ed7d47a3f8cb54c6c889b43718ab5d2a0ba261
nanayama moyoka <working@iseki.space>
2023-08-18 19:51:53 +0800

    feat(gradle): add DO_NOT_SCAN_GRADLE

--- module/gradle_init.go
+++ module/gradle_init.go
@@ -1,7 +1,13 @@
 package module
 
-import "github.com/murphysecurity/murphysec/module/gradle"
+import (
+	"github.com/murphysecurity/murphysec/module/gradle"
+	"os"
+)
 
 func init() {
+	if os.Getenv("DO_NOT_SCAN_GRADLE") == "1" {
+		return
+	}
 	Inspectors = append(Inspectors, &gradle.Inspector{})
 }
