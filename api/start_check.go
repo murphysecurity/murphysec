bd586e8eb88e676379d134ac729a5d73e88115a7
nanayama moyoka <working@iseki.space>
2024-01-26 11:21:19 +0800

    fix: 改一下错误输出

--- api/start_check.go
+++ api/start_check.go
@@ -7,8 +7,10 @@ func StartCheck(client *Client, task *model.ScanTask) error {
 	// 后端一定要我把 maven 参数再传一遍
 	var data = map[string]any{
 		"subtask_id":           task.SubtaskId,
-		"package_private_id":   task.MavenSourceId,
 		"package_private_name": task.MavenSourceName,
+	}
+	if task.MavenSourceId != "" {
+		data["package_private_id"] = task.MavenSourceId
 	}
 	return client.DoJson(client.PostJson(joinURL(client.baseUrl, "/platform3/v3/client/start_check"), data), nil)
 }
