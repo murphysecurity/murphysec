cdac37025ebad6f9022acef40aa04b3eda2001f9
nanayama moyoka <working@iseki.space>
2024-04-02 19:03:46 +0800

    perf: improve fragment scanning performance

--- inspector/dep_fragment.go
+++ inspector/dep_fragment.go
@@ -3,12 +3,13 @@ package inspector
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"github.com/murphysecurity/fix-tools/fix"
 	"github.com/murphysecurity/murphysec/infra/logctx"
 	"github.com/murphysecurity/murphysec/model"
 	"github.com/murphysecurity/murphysec/utils"
 	"github.com/murphysecurity/murphysec/utils/must"
+	"runtime"
+	"sync"
 )
 
 func scanFragment(ctx context.Context, dir string, components []model.Component) ([]model.ComponentCodeFragment, error) {
@@ -18,31 +19,47 @@ func scanFragment(ctx context.Context, dir string, components []model.Component)
 		return make([]model.ComponentCodeFragment, 0), nil
 	}
 	var result = make([]model.ComponentCodeFragment, 0)
-	for _, component := range components {
-		if component.Ecosystem != "maven" {
+	var wg sync.WaitGroup
+	var componentCh = make(chan model.Component, len(components))
+	for _, it := range components {
+		if it.Ecosystem != "maven" {
 			continue
 		}
-		var param = fix.FixParams{
-			ShowOnly: true,
-			CompList: []fix.Comp{{
-				CompName:    component.CompName,
-				CompVersion: component.CompVersion,
-			}},
-			PackageManager: "maven",
-			RepoType:       "local",
-			Dir:            dir,
-		}
-		logger.Sugar().Debugf("fix: %s", string(must.A(json.Marshal(param))))
-		xdResult := param.Fix()
-		if xdResult.Err != nil {
-			return nil, fmt.Errorf("scan fragment: %w", xdResult.Err)
-		}
-		var r = model.ComponentCodeFragment{
-			Component:          component,
-			CodeFragmentResult: xdResult,
-		}
-		result = append(result, r)
+		componentCh <- it
+	}
+	var resultCh = make(chan model.ComponentCodeFragment, len(components))
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer func() { wg.Done() }()
+			for component := range componentCh {
+				var param = fix.FixParams{
+					ShowOnly: true,
+					CompList: []fix.Comp{{
+						CompName:    component.CompName,
+						CompVersion: component.CompVersion,
+					}},
+					PackageManager: "maven",
+					RepoType:       "local",
+					Dir:            dir,
+				}
+				logger.Sugar().Debugf("fix: %s", string(must.A(json.Marshal(param))))
+				xdResult := param.Fix()
+				if xdResult.Err != nil {
+					logger.Sugar().Errorf("scan fragment: %s", xdResult.Err)
+				}
+				resultCh <- model.ComponentCodeFragment{
+					Component:          component,
+					CodeFragmentResult: xdResult,
+				}
+			}
+		}()
 	}
 
+	wg.Wait()
+	close(resultCh)
+	for ch := range resultCh {
+		result = append(result, ch)
+	}
 	return result, nil
 }
