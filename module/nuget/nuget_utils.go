58d41698497721e7fb9020622f1336bcf806f73b
ReverseDirectionGod <9086199+chen-haoxua@user.noreply.gitee.com>
2024-07-25 15:39:19 +0800

    style: reformat code

--- module/nuget/nuget_utils.go
+++ module/nuget/nuget_utils.go
@@ -12,32 +12,6 @@ func checkPackagesIsExistence(fileName string) bool {
 	return utils.IsFile(fileName)
 }
 
-// 判断nuget命令是否存在
-//
-//	func checkNugetCommand() bool {
-//		_, err := exec.LookPath("nuget")
-//		return err == nil
-//	}
-// func findCLN(dir string) (filePath string, err error) {
-
-// 	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		if info.IsDir() {
-// 			return nil
-// 		}
-// 		if filepath.Ext(path) == ".sln" {
-// 			// 找到.sln文件所在的目录
-// 			filePath = filepath.Dir(path)
-// 			return nil
-// 		}
-// 		return nil // 继续搜索
-// 	})
-
-//		return filePath, err
-//	}
 func findCLNList(dir string) (filePath []string, err error) {
 
 	err = filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
