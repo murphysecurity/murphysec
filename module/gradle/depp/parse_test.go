bbe5df1d98c778c0572401b38c098ea19b4c2e1a
ReverseDirectionGod <9086199+chen-haoxua@user.noreply.gitee.com>
2024-08-09 10:30:55 +0800

    fix: gradle skip useless lines

--- module/gradle/depp/parse_test.go
+++ module/gradle/depp/parse_test.go
@@ -1,12 +1,16 @@
 package depp
 
 import (
+	"bufio"
 	"bytes"
 	_ "embed"
 	"fmt"
-	"github.com/stretchr/testify/assert"
+	"io/ioutil"
 	"os"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 //go:embed parse_gradle_dep_testcase
@@ -49,4 +53,32 @@ func Test_parse_4(t *testing.T) {
 		printTree(data, 0, os.Stdout)
 	})
 	assert.NoError(t, e)
+}
+func Test_file(t *testing.T) {
+
+	filename := "C:\\Users\\陈浩轩\\Desktop\\mofei\\murphysec\\module\\gradle\\depp\\parse_gradle_dep_testcase4"
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	newContent := make([]byte, 0, len(content))
+	scanner := bufio.NewScanner(strings.NewReader(string(content)))
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		if len(line) > 180 {
+			newContent = append(newContent, line[180:]...)
+		} else {
+			newContent = append(newContent, line...)
+		}
+		newContent = append(newContent, '\n')
+	}
+	scanner.Err()
+	err = ioutil.WriteFile(filename, newContent, 0644)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	fmt.Println("Content has been updated.")
 }
