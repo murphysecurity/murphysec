788dfe0813fba01ce8039863462560a6bc9334d0
iseki <admin@iseki.space>
2024-12-09 12:03:47 +0800

    test(pnpm/v5): refactor test cases

--- module/pnpm/v5/v5_test.go
+++ module/pnpm/v5/v5_test.go
@@ -4,19 +4,31 @@ import (
 	"encoding/json"
 	"github.com/murphysecurity/murphysec/utils/must"
 	"github.com/stretchr/testify/assert"
+	"io"
 	"testing"
 )
 
-func TestParseLockfile(t *testing.T) {
-	for i, s := range testDataList {
-		lockfile, e := ParseLockfile([]byte(s))
-		assert.NoError(t, e, i)
-		assert.NotNil(t, lockfile, i)
+func TestLockfile(t *testing.T) {
+	files, _ := testFiles.ReadDir("testdata")
+	assert.NotEmpty(t, files)
+	for _, s := range files {
+		t.Run(s.Name(), func(t *testing.T) {
+			f, e := testFiles.Open("testdata/" + s.Name())
+			assert.NoError(t, e)
+			defer func() { assert.NoError(t, f.Close()) }()
+			data, e := io.ReadAll(f)
+			assert.NoError(t, e)
+			lockfile, e := ParseLockfile(data)
+			assert.NoError(t, e)
+			assert.NotNil(t, lockfile)
+
+		})
 	}
 }
 
 func TestBuildDepTree(t *testing.T) {
-	l, _ := ParseLockfile([]byte(testDataList[4]))
+
+	l, _ := ParseLockfile([]byte(testData5))
 	tree := BuildDepTree(l, nil, "")
 	assert.NotNil(t, tree)
 	t.Log(string(must.A(json.MarshalIndent(tree, "| ", " "))))
