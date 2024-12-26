fd1aafef71c45818ac1c2e1f1bdc588a68ed6038
反方向的神 <1446575944@qq.com>
2024-12-26 16:47:28 +0800

    fix: private source

--- module/go_mod/go.go
+++ module/go_mod/go.go
@@ -1,12 +1,7 @@
 package go_mod
 
 import (
-	"bufio"
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"os/exec"
 	"path/filepath"
 
 	"github.com/murphysecurity/murphysec/infra/logctx"
@@ -14,7 +9,6 @@ import (
 	"github.com/murphysecurity/murphysec/utils"
 	"github.com/pkg/errors"
 	"go.uber.org/zap"
-	"golang.org/x/mod/modfile"
 )
 
 type Inspector struct{}
@@ -53,100 +47,9 @@ func (Inspector) InspectProject(ctx context.Context) error {
 	return nil
 }
 
-func mapRequireToDependencyItem(it *modfile.Require) model.DependencyItem {
-	return model.DependencyItem{
-		Component: model.Component{
-			CompName:    it.Mod.Path,
-			CompVersion: it.Mod.Version,
-			EcoRepo:     EcoRepo,
-		},
-		IsDirectDependency: !it.Indirect,
-	}
-}
-
 var EcoRepo = model.EcoRepo{
 	Ecosystem:  "go",
 	Repository: "",
 }
 
 var _ErrGoNotFound = errors.New("go not found")
-
-func doGoList(ctx context.Context, dir string) (result []model.DependencyItem, e error) {
-	var logger = logctx.Use(ctx)
-	var cmd = exec.CommandContext(ctx, "go", "list", "-json", "-m", "all")
-	cmd.Dir = dir
-	stdout, e := cmd.StdoutPipe()
-	if e != nil {
-		e = fmt.Errorf("create stdout pipe failed: %w", e)
-		logger.Error(e.Error())
-		return
-	}
-	stderr, e := cmd.StderrPipe()
-	if e != nil {
-		e = fmt.Errorf("create stderr pipe failed: %w", e)
-		logger.Error(e.Error())
-		return
-	}
-	go func() {
-		defer func() { _ = stderr.Close() }()
-		var scanner = bufio.NewScanner(stderr)
-		scanner.Buffer(nil, 1024*4)
-		scanner.Split(bufio.ScanLines)
-		for scanner.Scan() {
-			logger.Warn("go: " + scanner.Text())
-		}
-	}()
-	logger.Sugar().Infof("executing command: %s", cmd)
-	var decoder = json.NewDecoder(stdout)
-	var scanner = bufio.NewScanner(stdout)
-	scanner.Buffer(nil, 1024*4)
-	scanner.Split(bufio.ScanLines)
-	e = cmd.Start()
-	if e != nil {
-		// if the command is not found, we should not return error
-		if errors.Is(e, exec.ErrNotFound) {
-			e = _ErrGoNotFound
-			return
-		}
-		e = fmt.Errorf("start command failed: %w", e)
-		logger.Error(e.Error())
-		return
-	}
-	defer func() {
-		if cmd.Process != nil {
-			_ = cmd.Process.Kill()
-			logger.Debug("process killed, waiting...")
-
-			_, _ = cmd.Process.Wait()
-			logger.Debug("after wait.")
-		}
-	}()
-	logger.Debug("start scanning...")
-	var m struct {
-		Path     string `json:"path"`
-		Version  string `json:"version"`
-		Indirect bool   `json:"indirect"`
-	}
-	for {
-		e = decoder.Decode(&m)
-		if e != nil {
-			break
-		}
-		result = append(result, model.DependencyItem{
-			Component: model.Component{
-				CompName:    m.Path,
-				CompVersion: m.Version,
-				EcoRepo:     EcoRepo,
-			},
-			IsDirectDependency: !m.Indirect,
-		})
-	}
-	if e != io.EOF {
-		e = fmt.Errorf("decode json failed: %w", e)
-		logger.Error(e.Error())
-		return
-	}
-	logger.Debug("done.")
-	_ = stdout.Close()
-	return
-}
