4968a10b52a32db9925be566cfb519fd1041f8e3
hundredbai <yubaichao2008@126.com>
2025-09-05 13:24:56 +0800

    fix: 支持centos6系统的repository获取

--- envinspection/osinfo_linux.go
+++ envinspection/osinfo_linux.go
@@ -1,12 +1,43 @@
 package envinspection
 
-import "github.com/zcalusic/sysinfo"
+import (
+	"io/ioutil"
+	"regexp"
+	"strings"
+
+	"github.com/zcalusic/sysinfo"
+)
 
 func getOsInfo() string {
 	var si sysinfo.SysInfo
 	si.GetSysInfo()
 	var vendor = si.OS.Vendor
 	var version = si.OS.Version
+	// centos/rhel 兼容补丁
+	if vendor == "" || vendor == "unknown" {
+		if content, err := ioutil.ReadFile("/etc/redhat-release"); err == nil {
+			line := strings.ToLower(string(content))
+			var re = regexp.MustCompile(`([a-zA-Z ]+) release ([0-9.]+)`)
+			if m := re.FindStringSubmatch(line); len(m) == 3 {
+				v := strings.TrimSpace(m[1])
+				switch {
+				case strings.Contains(v, "centos"):
+					vendor = "centos"
+				case strings.Contains(v, "red hat enterprise linux server"):
+					vendor = "rhel"
+				default:
+					vendor = strings.ReplaceAll(v, " ", "_")
+				}
+				// 只保留大版本号
+				ver := m[2]
+				if idx := strings.Index(ver, "."); idx > 0 {
+					version = ver[:idx]
+				} else {
+					version = ver
+				}
+			}
+		}
+	}
 	switch vendor {
 	case "opensuse-leap":
 		vendor = "opensuse:leap"
