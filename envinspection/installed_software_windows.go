9ebcf2978e45a22021812335e70b76d4add0b35e
iseki <admin@iseki.space>
2025-12-18 15:03:45 +0800

    feat: enhance pending patch detection with PowerShell and console encoding support

--- envinspection/installed_software_windows.go
+++ envinspection/installed_software_windows.go
@@ -3,17 +3,25 @@
 package envinspection
 
 import (
+	"bytes"
 	"context"
 	"fmt"
 	"os/exec"
 	"path/filepath"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/murphysecurity/murphysec/infra/logctx"
 	"github.com/murphysecurity/murphysec/model"
-	"github.com/repeale/fp-go"
 	"golang.org/x/sys/windows"
 	"golang.org/x/sys/windows/registry"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/charmap"
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/encoding/korean"
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/encoding/traditionalchinese"
 )
 
 type listSubKeysError struct {
@@ -87,12 +95,92 @@ func listInstalledSoftwareWindows(ctx context.Context) ([]model.DependencyItem, 
 	return r, nil
 }
 
+func consoleDecoder() *encoding.Decoder {
+	var cp, e = windows.GetConsoleCP()
+	if e != nil {
+		return nil
+	}
+	switch cp {
+	case 936:
+		return simplifiedchinese.GBK.NewDecoder()
+	case 932:
+		return japanese.ShiftJIS.NewDecoder()
+	case 950:
+		return traditionalchinese.Big5.NewDecoder()
+	case 949:
+		return korean.EUCKR.NewDecoder()
+	case 437:
+		return charmap.CodePage437.NewDecoder()
+	case 850:
+		return charmap.CodePage850.NewDecoder()
+	case 852:
+		return charmap.CodePage852.NewDecoder()
+	case 866:
+		return charmap.CodePage866.NewDecoder()
+	case 1250:
+		return charmap.Windows1250.NewDecoder()
+	case 1251:
+		return charmap.Windows1251.NewDecoder()
+	case 1252:
+		return charmap.Windows1252.NewDecoder()
+	default:
+		return nil // assume UTF-8
+	}
+}
+
 func listPendingPatch(ctx context.Context) []string {
 	var logger = logctx.Use(ctx).Sugar()
-	data, e := exec.CommandContext(ctx, "wmic", "qfe", "get", "HotFixID").Output()
+	ctx, cancel := context.WithTimeout(ctx, time.Minute*5)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, "powershell")
+	cmd.Stdin = bytes.NewReader([]byte(`
+$session = New-Object -ComObject Microsoft.Update.Session
+$searcher = $session.CreateUpdateSearcher()
+$searcher.Online = $true
+$resultAvailable = $searcher.Search("IsInstalled=0")
+function Get-KBString($update) {
+    if ($update.KBArticleIDs -and $update.KBArticleIDs.Count -gt 0) {
+        return "KB$($update.KBArticleIDs[0])"
+    }
+    return ""
+}
+function Print-Updates($updates) {
+    foreach ($update in $updates) {
+        $kb = Get-KBString $update
+        Write-Host "KBNumber: $kb"
+    }
+}
+Print-Updates $resultAvailable.Updates
+
+
+`))
+	var kbPattern = regexp.MustCompile(`KBNumber: (KB\d+)`)
+	var output bytes.Buffer
+	cmd.Stdout = &output
+	var stderr bytes.Buffer
+	cmd.Stderr = &stderr
+	e := cmd.Run()
 	if e != nil {
-		logger.Warnf("wmic qfe get HotFixID failed: %s", e)
+		logger.Errorf("powershell failed: %s", e)
+		return nil
 	}
-	var isKB = func(s string) bool { return strings.HasPrefix(s, "KB") }
-	return fp.Pipe2(fp.Map(strings.TrimSpace), fp.Filter(isKB))(strings.Split(string(data), "\n"))
+	stdoutBytes := output.Bytes()
+	stderrBytes := stderr.Bytes()
+	converter := consoleDecoder()
+	if converter != nil {
+		stdoutBytes, _ = converter.Bytes(stdoutBytes)
+		stderrBytes, _ = converter.Bytes(stderrBytes)
+	}
+	stderrText := strings.TrimSpace(string(stderrBytes))
+	if stderrText != "" {
+		logger.Errorf("powershell failed: %s", stderrText)
+	}
+	var kbNumbers []string
+	for line := range strings.SplitSeq(strings.TrimSpace(string(stdoutBytes)), "\n") {
+		var kbNumber = kbPattern.FindStringSubmatch(line)
+		if kbNumber != nil {
+			kbNumbers = append(kbNumbers, kbNumber[1])
+		}
+	}
+	return kbNumbers
 }
