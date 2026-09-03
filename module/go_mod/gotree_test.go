c0cf02ff4e307d4425d573e1970ae6852273ced6
李川 <2890792376@qq.com>
2026-09-03 19:52:11 +0800

    test(go): cover version comparison edge cases

--- module/go_mod/gotree_test.go
+++ module/go_mod/gotree_test.go
@@ -27,15 +27,33 @@ func TestHigherVersion(t *testing.T) {
 			want:     "v1.3.0",
 		},
 		{
-			name:     "release is higher than prerelease",
+			name:     "semantically equal versions keep first",
+			version1: "v1.2.3+incompatible",
+			version2: "v1.2.3",
+			want:     "v1.2.3+incompatible",
+		},
+		{
+			name:     "release is kept when prerelease is second",
 			version1: "v2.0.0",
 			version2: "v2.0.0-rc.1",
 			want:     "v2.0.0",
 		},
 		{
-			name:     "pseudo-version comparison",
+			name:     "release is selected when prerelease is first",
+			version1: "v2.0.0-rc.1",
+			version2: "v2.0.0",
+			want:     "v2.0.0",
+		},
+		{
+			name:     "newer pseudo-version is selected when second",
 			version1: "v0.0.0-20240101000000-aaaaaaaaaaaa",
 			version2: "v0.0.0-20240201000000-bbbbbbbbbbbb",
+			want:     "v0.0.0-20240201000000-bbbbbbbbbbbb",
+		},
+		{
+			name:     "newer pseudo-version is kept when first",
+			version1: "v0.0.0-20240201000000-bbbbbbbbbbbb",
+			version2: "v0.0.0-20240101000000-aaaaaaaaaaaa",
 			want:     "v0.0.0-20240201000000-bbbbbbbbbbbb",
 		},
 	}
